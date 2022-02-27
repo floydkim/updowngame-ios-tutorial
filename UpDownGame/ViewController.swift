a4467e23fb1b04dce716e19f892f9a78b364ee3f
floydkim <for12gm@gmail.com>
2022-02-28 00:20:38 +0900

    슬라이더와 버튼 추가

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
 
 }
 
