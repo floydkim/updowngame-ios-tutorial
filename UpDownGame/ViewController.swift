700290d3e0502edb4b4dc884aa032bed8445592b
floydkim <for12gm@gmail.com>
2022-03-01 20:31:07 +0900

    hit 버튼 이벤트 핸들러 / reset 함수

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,27 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         slider.setThumbImage(UIImage(named: "slider_thumb"), for: .normal)
+        
+        reset()
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
     }
 
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+//        tryCountLabel.text = String(tryCount) + " / 5"
+        tryCountLabel.text = "\(tryCount) / 5" // string interpolation
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
-        // TODO: slider를 초기값으로 되돌리기
-        // TODO: 진행중이던 게임 데이터를 초기화하기
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 
 }
