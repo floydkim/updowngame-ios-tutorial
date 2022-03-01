bce3541357005c9336e3fd330b3d71034486d806
floydkim <for12gm@gmail.com>
2022-03-01 21:15:14 +0900

    크레딧화면 닫기 구현

--- UpDownGame/CreditViewController.swift
+++ UpDownGame/CreditViewController.swift
@@ -15,6 +15,9 @@ class CreditViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil) // present와 반대
+    }
 
     /*
     // MARK: - Navigation
